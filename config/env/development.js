9d0a534a458741bd7ee5907f824c0d270452b743
Brandon Garcia <brandongarcia2332@gmail.com>
2016-01-31 16:45:32 -0800

    Twitter oauth added, facebook signin homepage still needs to be fixed

--- config/env/development.js
+++ config/env/development.js
@@ -5,5 +5,10 @@ module.exports = {
 		clientID: '1662220374034586',
 		clientSecret: 'aaf71423d15971f1ae4727e7396bbabb',
 		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+	},
+	twitter:{
+		clientID: 'nagmi1fei5rfNTD6RyFXjE2OA',
+		clientSecret: 'yo720fRMEREGvcpMvrenkma6arKPwDRqnm1LchwbyKgrNpNCHQ',
+		callbackURL: 'http://localhost:3000/oauth/twitter/callback'
 	}
 };
