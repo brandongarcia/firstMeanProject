06ad7567e63d69e5afc2b4285d0534f61bfae63e
Brandon Garcia <brandongarcia2332@gmail.com>
2016-01-31 18:03:55 -0800

    twitter and google oauth added; facebook home loging still buggy

--- config/env/development.js
+++ config/env/development.js
@@ -10,5 +10,10 @@ module.exports = {
 		clientID: 'nagmi1fei5rfNTD6RyFXjE2OA',
 		clientSecret: 'yo720fRMEREGvcpMvrenkma6arKPwDRqnm1LchwbyKgrNpNCHQ',
 		callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+	},
+	google:{
+		clientID: '660632790174-rr88m0sd6vteeigk6gkk0v08jrifkb7j.apps.googleusercontent.com',
+		clientSecret: 'HKOchme_zT8vBvnCz-RYBsDb',
+		callbackURL: 'http://localhost:3000/oauth/google/callback'
 	}
 };
